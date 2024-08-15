ace6c4aef6abf22eb208db412082da15e8207e5c
cybergaz <kkanttechy@gmail.com>
2024-08-15 23:22:47 +0530

    update: about

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -11,69 +11,69 @@ import vueIcon from "../../assets/vue-icon.svg";
 import ScrollAnimation from "react-animate-on-scroll";
 
 
-export function About(){
-  return(
-    <Container id="sobre">
-      <div className="about-text">
-        <ScrollAnimation animateIn="fadeInLeft">
-          <h2>About me</h2>
-        </ScrollAnimation>
-        <ScrollAnimation animateIn="fadeInLeft" delay={0.2 * 1000}>
-          <p>My name is Krishan Kant, I am a computer science student at GLA university currently in 3rd year. I am passionate about delivering solutions that aggregate in people's lives and at the same time challenge me. Improve my skills as a Front-End and Back-End developer</p>
-        </ScrollAnimation>
-        <ScrollAnimation animateIn="fadeInLeft" delay={0.4 * 1000} style={{marginTop: "2rem", marginBottom: "2rem"}}>
+export function About() {
+    return (
+        <Container id="sobre">
+            <div className="about-text">
+                <ScrollAnimation animateIn="fadeInLeft">
+                    <h2>About me</h2>
+                </ScrollAnimation>
+                <ScrollAnimation animateIn="fadeInLeft" delay={0.2 * 1000}>
+                    <p>My name is Krishan Kant, I am a computer science graduate, passionate about delivering solutions that aggregate in people's lives and at the same time challenge me. Improve my skills as a Front-End and Back-End developer</p>
+                </ScrollAnimation>
+                <ScrollAnimation animateIn="fadeInLeft" delay={0.4 * 1000} style={{ marginTop: "2rem", marginBottom: "2rem" }}>
 
-          <p>I develop websites and applications using HTML, CSS, and JavaScript. I am familiar with developing layouts that provide me. I am always improving myself with every project they put in my hands.</p>
-        </ScrollAnimation>
-        <ScrollAnimation animateIn="fadeInLeft" delay={0.6 * 1000}>
+                    <p>I develop websites and applications using HTML, CSS, and JavaScript. I am familiar with developing layouts that provide me. I am always improving myself with every project they put in my hands.</p>
+                </ScrollAnimation>
+                <ScrollAnimation animateIn="fadeInLeft" delay={0.6 * 1000}>
 
-          <p>I am a dedicated person who runs behind to fulfill his dreams, worker and results-oriented, I always seek to achieve my best version.</p>
-        </ScrollAnimation>
+                    <p>I am a dedicated person who runs behind to fulfill his dreams, worker and results-oriented, I always seek to achieve my best version.</p>
+                </ScrollAnimation>
 
-        <ScrollAnimation animateIn="fadeInLeft" delay={0.7 * 1000}>
-          <h3>Here are my main skills:</h3>
-        </ScrollAnimation>
-        <div className="hard-skills">
+                <ScrollAnimation animateIn="fadeInLeft" delay={0.7 * 1000}>
+                    <h3>Here are my main skills:</h3>
+                </ScrollAnimation>
+                <div className="hard-skills">
 
-          <div className="hability">
-            <ScrollAnimation animateIn="fadeInUp" delay={0.1 * 1000}>
-              <img src={reactIcon} alt="React" />
-            </ScrollAnimation>
-          </div>
+                    <div className="hability">
+                        <ScrollAnimation animateIn="fadeInUp" delay={0.1 * 1000}>
+                            <img src={reactIcon} alt="React" />
+                        </ScrollAnimation>
+                    </div>
 
-          <div className="hability">
-          <ScrollAnimation animateIn="fadeInUp" delay={0.3 * 1000}> 
-            <img src={jsIcon} alt="JavaScript" />
-          </ScrollAnimation>
-          </div>
+                    <div className="hability">
+                        <ScrollAnimation animateIn="fadeInUp" delay={0.3 * 1000}>
+                            <img src={jsIcon} alt="JavaScript" />
+                        </ScrollAnimation>
+                    </div>
 
-          <div className="hability">
-          <ScrollAnimation animateIn="fadeInUp" delay={0.4 * 1000}> 
-            <img src={htmlIcon} alt="Html" />
-          </ScrollAnimation>
-          </div>
+                    <div className="hability">
+                        <ScrollAnimation animateIn="fadeInUp" delay={0.4 * 1000}>
+                            <img src={htmlIcon} alt="Html" />
+                        </ScrollAnimation>
+                    </div>
 
-          <div className="hability">
-          <ScrollAnimation animateIn="fadeInUp" delay={0.5 * 1000}> 
-            <img src={cssIcon} alt="Css" />
-          </ScrollAnimation>
-          </div>
-          <div className="hability">
-          <ScrollAnimation animateIn="fadeInUp" delay={0.6 * 1000}> 
-            <img src={nodeIcon} alt="Node" />
-          </ScrollAnimation>
-          </div>
+                    <div className="hability">
+                        <ScrollAnimation animateIn="fadeInUp" delay={0.5 * 1000}>
+                            <img src={cssIcon} alt="Css" />
+                        </ScrollAnimation>
+                    </div>
+                    <div className="hability">
+                        <ScrollAnimation animateIn="fadeInUp" delay={0.6 * 1000}>
+                            <img src={nodeIcon} alt="Node" />
+                        </ScrollAnimation>
+                    </div>
 
-          <div className="hability">
-          <ScrollAnimation animateIn="fadeInUp" delay={0.7 * 1000}>
-            <img src={typescriptIcon} alt="Typescript" />
-          </ScrollAnimation>
-          </div>
+                    <div className="hability">
+                        <ScrollAnimation animateIn="fadeInUp" delay={0.7 * 1000}>
+                            <img src={typescriptIcon} alt="Typescript" />
+                        </ScrollAnimation>
+                    </div>
 
 
-        </div>
-      </div>
-      
-    </Container>
-  )
+                </div>
+            </div>
+
+        </Container>
+    )
 }
