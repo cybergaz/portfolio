f0151b19179606350e17519d4a7260973fb423d2
cybergaz <kkanttechy@gmail.com>
2024-12-24 19:08:03 +0530

    chore: link and readme update

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
-# Portfolio Website
+<div align=center>
+
+# Portfolio website (old)
+
+#### migrated from [old portfolio](https://cybergaz-old.vercel.app/) to [new portfolio](https://cybergaz.vercel.app/)
+
+</div>
 
 
 ## Tecnologias used:
--- src/components/Announcement/Announcement.tsx
+++ src/components/Announcement/Announcement.tsx
@@ -7,7 +7,7 @@ export function Announcement() {
             <div className="hero-text">
                 This portfolio have been migrated to a new one
             </div>
-            <a className="link" href="https://cybergazv2.vercel.app/" target="_blank">Visit New Portfolio</a>
+            <a className="link" href="https://cybergaz.vercel.app/" target="_blank">Visit New Portfolio</a>
             <div>in any case the link to the portfolio is not working, visit source code and navigate from there</div>
             <a className="link" href="https://github.com/cybergaz/cybergaz.me" target="_blank">Github</a>
             <span className="line"></span>
